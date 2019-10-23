5f7c5ee7ae2680cb31570b88646deffd6ed1768a
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-23 12:27:32 +0200

    Add project files.

---
+++ Triangoli/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci il primo lato");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci il secondo lato");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci il terzo lato");
+            double c = double.Parse(Console.ReadLine());
+            double perimetro = 0;
+            double area = 0;
+            double altezza = Math.Sqrt(3) / 2 * a;
+            if (a == b && a == c)
+            {
+                Console.WriteLine("è un triangolo equilatero");
+                perimetro = (a + b + c);
+                area = (altezza * a) / 2;
+                Console.WriteLine($"il perimetro del triangolo è {perimetro} e l'area è {area}");
+            }
+            else
+            {
+                Console.WriteLine("non è un trangolo equilatero");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
